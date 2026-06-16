481ef99e0aaadd3ff5cdcc053195b7642db35d0d
BAKSHAM AKASH REDDY <kudisetimadhu@MacBook-Air.local>
2026-06-16 09:56:29 +0530

    fix:resolved all prisma and types issues

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { db: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const db =
-  globalForPrisma.db ||
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   })
-export const prisma = db
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.db = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
